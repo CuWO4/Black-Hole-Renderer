2904f4bd0f2b1f5522a66fc88c343b710d1a1548
CuWO4 <wutong.tony@outlook.com>
2025-03-11 19:11:43 +0800

    util: implement progress bar

---
+++ utils/bar.hpp
@@ -0,0 +1,37 @@
+#ifndef BAR_HPP__
+#define BAR_HPP__
+
+#include <cstdio>
+
+class Bar {
+public:
+
+  Bar(int total) 
+    : total(total), current(0), last(0), 
+      per_cell((total + total_characters - 1) / total_characters) {
+    putchar('[');
+    for (int i = 0; i < total_characters; i++) {
+      putchar(' ');
+    }
+    putchar(']');
+    printf("\x1b[2G");
+  }
+
+  void step() {
+    current++;
+    if (current > last * per_cell) {
+      putchar('#');
+      last++;
+    }
+  }
+
+private:
+  static constexpr int total_characters = 50;
+
+  int total;
+  int current;
+  int last;
+  int per_cell;
+};
+
+#endif
