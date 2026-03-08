d569f66cff58becf5f8c85fd87f0421f4b52549d
CuWO4 <wutong.tony@outlook.com>
2026-03-08 12:30:48 +0800

    fix: strictly adhere to the relativistic effect

--- schwarz_metric_ray.cpp
+++ schwarz_metric_ray.cpp
@@ -4,17 +4,19 @@ SM_Ray::SM_Ray(Vec3 start, Vec3 direction, Vec3 black_hole_pos)
   : Ray{ start, direction }, black_hole_pos(black_hole_pos) {}
 
 void SM_Ray::step(float dl) {
+  // r, position, dl are all in units of Rs (dimensionless length unit)
   Vec3 r = black_hole_pos - position;
+  float r2 = r.l2();
+  if (r2 < 1e-12f) {
+    position += dl * direction;
+    return;
+  }
   Vec3 normal = r.unit();
 
   float h2 = (r ^ direction).l2();
 
-  // dv/ds = 3GMh^2 / c^2 r^4
-  Vec3 a = 3 * constant::physics::G * constant::model::M / constant::physics::c / constant::physics::c
-    * constant::physics::M_Sun  * h2 / r.l2() / r.l2()
-    / 3.7e10f /* some magic number for calibrating the unit system */
-    * normal;
-  Vec3 dv = a * dl;
+  // In Rs-units: 3GM/c^2 = (3/2) * Rs
+  Vec3 dv = (1.5f * h2 / (r2 * r2)) * normal * dl;
 
   direction = (direction + dv).unit();
   position += dl * direction;
