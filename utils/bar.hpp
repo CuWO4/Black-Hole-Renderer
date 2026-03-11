ec35274cb8ec54dfe0db1408886c3ee39d9ffcdc
CuWO4 <wutong.tony@outlook.com>
2026-03-11 23:45:44 +0800

    fix: fix the issue where the progress bar was shorter than expected

--- utils/bar.hpp
+++ utils/bar.hpp
@@ -1,14 +1,14 @@
 #ifndef BAR_HPP__
 #define BAR_HPP__
 
+#include <algorithm>
 #include <cstdio>
 
 class Bar {
 public:
 
   Bar(int total) 
-    : current(0), last(0), 
-      per_cell((total + total_characters - 1) / total_characters) {
+    : total(std::max(total, 0)), current(0), rendered(0) {
     putchar('[');
     for (int i = 0; i < total_characters; i++) {
       putchar(' ');
@@ -18,25 +18,33 @@ public:
   }
 
   void step() {
-    current++;
-    if (current > last * per_cell) {
-      putchar('#');
-      std::fflush(stdout);
-      last++;
+    if (current >= total) {
+      return;
     }
+
+    current++;
+    const int target = total == 0
+      ? total_characters
+      : (current * total_characters) / total;
+
+    while (rendered < target) {
+      putchar('#');
+      rendered++;
+    }
+
+    std::fflush(stdout);
   }
 
   ~Bar() {
-    putchar(']');
-    putchar('\n');
+    std::printf("\x1b[%dG\n", total_characters + 3);
   }
 
 private:
   static constexpr int total_characters = 50;
 
+  int total;
   int current;
-  int last;
-  int per_cell;
+  int rendered;
 };
 
 #endif
